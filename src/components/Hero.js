167ccfcf000d301bd5cc5eb115c71e2640b3444b
lord <konadulordkweku@gmai.com>
2023-11-17 11:33:15 +0000

    add mobile query

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,14 +6,14 @@ function Hero() {
                 <div className="hero__text">
                     <div class="hero__text--primary">
                         <h1 className="hero__text--heading">Hello, I’m Lord Konadu</h1>
-                        <p>I'm a Full-Stack Developer based in Ghana, dedicated to creating immersive and visually appealing web applications through meticulously crafted code and a user-centric design approach.</p>
+                        <p>A Full-Stack Developer based in Ghana, dedicated to creating immersive and visually appealing web applications through meticulously crafted code and a user-centric design approach.</p>
                         <a href="#" className="btn__normal">Say Hello</a>
                     </div>
                     <div className="hero__text--secondary">
                         <img src={stacks} alt="hero" />
                     </div>
                 </div>
-                <div className="hero__image">
+                <div className="hero__image hidden">
                     <img src={Man2} alt="hero" />
                 </div>
             </div>
