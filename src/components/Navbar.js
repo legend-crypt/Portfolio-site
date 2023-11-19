c9e5db16129d5e9dcde9f1c5b87d63fd129a33c9
lord <yrnlehjend@gmail.com>
2023-11-19 00:11:27 +0000

    add mobile toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,31 @@
-import logo from '../assets/images/Logo_name.svg'
+import logo from '../assets/images/Logo_name.svg';
+import closeIcon from '../assets/images/mingcute_close-fill.svg';
+import hamburger from '../assets/images/ci_hamburger.svg';
+import { useState } from 'react';
+
+
 function Navbar({showContactButton}) {
+    const [clicked, setClicked] = useState(false)
+
+    const hamburgerClicked = () => {
+        if (!clicked) {
+            setClicked(true)
+        }
+        else {
+            setClicked(false)
+        }
+
+    }
+
+
     return (
-        <header className="header container">
-            <div class="row">
+        <header className="header">
+            <div class="row container">
                 <a href="#" className="nav__logo">
                     <img src={logo} className="logo" alt="logo" />
                 </a>
                 <nav className="nav">
-                    <ul className="nav__list">
+                    <ul id="nav__list" className={clicked? "#nav_list active": "#nav_list"}>
                         <a href="">
                             <li className= "nav__list--text col">Home</li>
                         </a>
@@ -27,6 +45,11 @@ function Navbar({showContactButton}) {
                         </a>
                         )}
                     </ul>
+
+                    <div>
+                        <button className="mobile_toggle" onClick={hamburgerClicked}><img src={clicked? closeIcon: hamburger }></img></button>
+
+                    </div>
                 </nav>
             </div>
         </header>
